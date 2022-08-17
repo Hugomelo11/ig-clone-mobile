5bd7187df90d1432b1c5c64817308e2c9f4b091a
Hugo Salles <salles.hugo05@gmail.com>
2022-08-17 15:53:27 -0400

    adding location, name and icon

--- components/PhotoCard.jsx
+++ components/PhotoCard.jsx
@@ -1,31 +1,36 @@
 import { View, Image, Text } from "react-native";
 import { photoCardStyles } from "../assets/styles";
-import { Avatar, Icon  } from 'react-native-elements'
-
+import { Avatar, Icon } from "react-native-elements";
 
 function PhotoCard({ photo }) {
-    
-    const BASE_URL = 'https://source.unsplash.com/random?miami='
+  const BASE_URL = "https://source.unsplash.com/random?miami=";
 
   return (
     <>
       <View style={photoCardStyles.cardContainer}>
         <View style={photoCardStyles.cardHeader}>
           {/* avatar */}
-        <Avatar rounded source={{uri: 'https://randomuser.me/api/portraits/men/1.jpg'}}/>
-        
+          <Avatar
+            rounded
+            source={{ uri: "https://randomuser.me/api/portraits/men/1.jpg" }}
+          />
 
           <View style={photoCardStyles.cardHeaderText}>
-            <Text> Header Text </Text>
-            <Text style={photoCardStyles.cardLocation}> location </Text>
+            <Text> D.J. Khaled </Text>
+            <Text style={photoCardStyles.cardLocation}> Miami </Text>
           </View>
-
         </View>
-        <Icon name='code' />
+        <Icon name="code" />
       </View>
 
-      <Image source={{uri: BASE_URL + 1}} style={photoCardStyles.cardImage} />
-      <Text style={photoCardStyles.cardText}> text here </Text>
+      <Image source={{ uri: BASE_URL + 1 }} style={photoCardStyles.cardImage} />
+      <Text style={photoCardStyles.cardText}>
+        {" "}
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo,
+        veritatis dolor nesciunt deleniti aspernatur blanditiis quis ipsum esse
+        eum tempore. Est explicabo accusamus, ipsum asperiores aliquid
+        laboriosam. Numquam, iusto consectetur!{" "}
+      </Text>
     </>
   );
 }
--- screens/Login.jsx
+++ screens/Login.jsx
@@ -1,12 +1,14 @@
 
 import { View, Text } from "react-native";
 import { loginStyles } from "./../assets/styles";
+import { Input } from 'react-native-elements'
 
 
 function Login() {
   return (
     <View style={loginStyles.headerBar}>
-      <Text style={loginStyles.headerText}> IG Clone</Text>
+      <Text style={loginStyles.headerText}> BocaGram</Text>
+
     </View>
   );
 }
